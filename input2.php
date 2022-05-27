77f3c887641ea8b8c4f7be862ca277eaebecb62f
evnastik <evnastik@gmail.com>
2022-05-28 01:32:03 +0500

    Update input2.php

--- input2.php
+++ input2.php
@@ -1,1 +1,31 @@
+<?php
+if(isset($_POST["firstname"]) && isset($_POST["lastname"]))
+{
+    $firstname = htmlentities($_POST["firstname"]);
+    $lastname = htmlentities($_POST["lastname"]);
+    $host = 'localhost';
+    $user = 'admin';
+    $password = '';
+    $db_name = 'test';
 
+    $link = mysqli_connect($host, $user, $password, $db_name) or die(mysqli_error());
+    $query = "INSERT INTO users (name, lastname) VALUES ('$firstname', '$lastname')";
+    $result = mysqli_query($link, $query) or die( mysqli_error($link) );
+    $output ="
+    <html>
+    <head>
+    <title>ОБЯЗАТЕЛЬНОЕ ДЗ</title>
+    </head>
+    <body>
+    Привет, ($firstname) ($lastname) <br />
+    Даные сохранены в БД.
+    <br />
+    <a href='/index.php'>Назад</a>
+    </body></html>";
+    echo $output;
+}
+else
+{
+    echo "Введенные данные некорректны";
+}
+?>
